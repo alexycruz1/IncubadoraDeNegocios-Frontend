aaa21d0373e1c6292abbfcfdfc123c895f7838a0
manuelpc97 <mcanelo41@gmail.com>
2017-09-12 04:52:28 -0600

    Message service corrected

--- proyecto/src/services/messageService.js
+++ proyecto/src/services/messageService.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
-var url = 'http://localhost:8000/IDN';
+var url = 'http://localhost:8000/IDN/';
 
 export default{
 	createMessage(newMessage){
